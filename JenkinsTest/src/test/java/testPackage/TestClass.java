83754b81278801e163702beca1b4153e7f5f979a
rohit <rohit@DESKTOP-PUIDNO7.mshome.net>
2022-04-13 00:50:25 +0200

    first commit

---
+++ JenkinsTest/src/test/java/testPackage/TestClass.java
@@ -0,0 +1,13 @@
+package testPackage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestClass {
+	
+	@Test
+	public void test()
+	{
+		Assert.assertEquals(false, false);
+	}
+
+}
